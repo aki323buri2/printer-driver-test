cb16c8f8717e1c947591d4c621910868e35b2675
中島　亮三 <aki323buri2@gmail.com>
2019-04-15 06:27:18 +0900

    .

---
+++ bin/installPrinterDriver.ps1
@@ -0,0 +1,21 @@
+$OutputEncoding = 'utf-8'
+$ipaddress = '172.16.11.115'
+$infPrinterName = 'FX DocuCentre-IV 7080'
+$printerName = 'DocuCentre-IV 7080'
+$infFilename = Join-Path $PSScriptRoot '..\x64\FXLV4JL.inf' 
+$infFilename = Convert-Path $infFilename
+echo $infFilename
+$location =  'C:\Windows\System32\Printing_Admin_Scripts\ja-JP'
+Set-Location $location
+
+$portName = 'IP_' + $ipaddress
+cscript prnport.vbs -a -r $portName -h $ipaddress -o raw -n 9100
+
+$os = Get-WmiObject -Class Win32_OperatingSystem
+
+if ( $os.OSarchitecture -eq '64 ƒrƒbƒg' )
+{
+    cscript prndrvr.vbs -a -m $infPrinterName -v 3 -i $infFilename
+}
+
+cscript prnmngr.vbs -a -p $printerName -m $infPrinterName -r $portName
---
+++ bin/runInstallPrinterDriver.bat
@@ -0,0 +1,1 @@
+powershell -NoProfile -ExecutionPolicy Unrestricted "D:\data\documents\printer` driver\script\bin\installPrinterDriver.ps1"
